43deb2ea3d1bbda9b6bbc2cddd3c5aa17f4ab5b2
Antonio <antoniorv6@Antonios-Mac-mini.local>
2022-09-23 12:19:04 -0700

    Reimplemented server with whisper

--- server/Wav2Vec.py
+++ server/Wav2Vec.py
@@ -1,11 +1,11 @@
-from transformers import pipeline
+import whisper
 from loguru import logger
 class AudioTranscription:
     @logger.catch
     def __init__(self, model_name) -> None:
-        self.pipe = pipeline(model=model_name)
+        self.model = whisper.load_model("base")
         
     @logger.catch            
     def __call__(self, audio_path):
-        output = self.pipe(audio_path, chunk_length_s=10, stride_length_s=(4, 2))
+        output = self.model.transcribe(audio_path)
         return output
