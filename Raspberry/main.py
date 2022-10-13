45197141451bdf42c4199f49b32a1c1dc679fe01
Alberto Berenguer Pastor <alberto.berenguer@ua.es>
2022-10-13 13:32:13 +0200

    Raspberry alert Family

--- Raspberry/main.py
+++ Raspberry/main.py
@@ -1,3 +1,4 @@
+from urllib import request
 from gpiozero import Button
 import pygame
 import time
@@ -9,6 +10,7 @@ import wavio as wv
 from base64 import b64encode
 import numpy as np
 import os
+import requests
 
 # Config usuario
 user_id = 'yERXYCKKtDN3b9aXNip4s9GWS1z1'
@@ -27,6 +29,24 @@ channel = connection.channel()
 # Creamos cola con el id del paciente
 channel.exchange_declare(exchange=user_id,
                          exchange_type='fanout')
+
+def alertFamily():
+    headers={'Accept': 'application/json', 
+        'Authorization': 'key=AAAAoZTf-J0:APA91bHCsxJymN-5ZVFPYzWJQ5ZaEBFC2gWhs2DLutcP6flxMVxTaZKXXVqNxtaUKeOOTgcJo5RTlJiZZGgjZDnR5jFtAFR_slJ_d9Gpf_eWzMUKSLEWvJ2mBl2oaOxE2DyjULNefI8r'
+    }
+    data = {
+    "to":"cKVVzWkqRhCdM2bAvN7T_h:APA91bGaSOjJOsm-JLgNFfep-1pOOnUyGti7q2Sh5cgDYE0MNJk_pIJ_4nW5UNbQC7vnEs4qGnN21-RdxdzA7b0Zwx-1pyH1ebbK0oCO4z90TE-G3BM1AuTIULC7tWwIfU1HCRDQ-YDX",
+    "notification":{
+        "title":"HelpVoice! - Nueva alerta",
+        "body":"Alberto Berenguer"
+    },
+    "data":{
+        "lat":40.4477155,
+        "lon": -3.6954323
+    }
+}
+    request.post('https://fcm.googleapis.com/fcm/send', data = data,
+    headers = headers)
 
 def playsound(file):
     pygame.mixer.music.load(audio_path + file)
@@ -70,6 +90,7 @@ def action():
             data['audio'] = enc.decode('utf-8')
             sendMessage(data)
             playsound(audios['ok'])
+            alertFamily()
             break
 
 
